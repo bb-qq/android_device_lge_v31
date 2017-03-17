90a90f1e975d73f5e69c0f26ee5e56d130e8b618
bb_qq <bb-qq@users.noreply.github.com>
2017-03-17 06:25:34 -0700

    Added device config for LGV31.

---
+++ README.md
@@ -0,0 +1,18 @@
+Copyright 2014 - The CyanogenMod Project
+
+Device configuration for LGE G3 VoLTE (KDDI)
+=====================================
+
+Basic   | Spec Sheet
+-------:|:-------------------------
+CPU     | Quad-core 2.5 GHz Krait 400
+CHIPSET | Qualcomm MSM8975AC Snapdragon 801
+GPU     | Adreno 330
+Memory  | 3GB RAM
+Shipped Android Version | 4.4
+MicroSD | Up to 128GB
+Battery | 3000 mAh
+Dimensions | 146.3 x 74.7 x 8.9 mm
+Display | 1440 x 2560 pixels True HD-IPS
+Camera  | 13 MP, 4160 x 3120 pixels
+Release Date | ?
---
+++ cm.mk
@@ -0,0 +1,17 @@
+$(call inherit-product, device/lge/v31/full_v31.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Enhanced NFC
+#$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+
+PRODUCT_NAME := cm_v31
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_DEVICE="g3" \
+    PRODUCT_NAME="g3_kddi" \
+    BUILD_FINGERPRINT="lge/g3_kddi/g3:6.0/MRA58K/160141503d43c:user/release-keys" \
+    PRIVATE_BUILD_DESC="g3_kddi-user 6.0 MRA58K 160141503d43c release-keys"
+
+PRODUCT_GMS_CLIENTID_BASE := android-verizon
